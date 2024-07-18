7f74acd280ef0cbe3c3a25dd452772e46219e782
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-07-18 13:57:23 -0400

    makig th e 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Gett.. and sett.. for attribute
+  get floors() {
+    return this._floors;
+  }
+
+  // Override the evacuationWa... method
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors.`;
+  }
+}
