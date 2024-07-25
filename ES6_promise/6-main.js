4c6e4c416ae5191a7eef11d5140b12ad978be460
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-07-25 11:36:13 -0400

    making some changes on the main 6

--- ES6_promise/6-main.js
+++ ES6_promise/6-main.js
@@ -1,3 +1,3 @@
 import handleProfileSignup from './6-final-user';
 
-console.log(handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg'));
+handleProfileSignup('Bob', 'Dylan', 'bob_dylan.jpg').then(console.log);
