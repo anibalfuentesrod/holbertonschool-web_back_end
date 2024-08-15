b29bdfe77128942ca0b070cb9346b80e42ed2300
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-15 09:59:12 -0400

    making some Union on the code to change the to correct anwer

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """same thing mannnnn"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[int]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """return the sum of floats and int and returns on floats"""
     return sum(mxd_lst)
