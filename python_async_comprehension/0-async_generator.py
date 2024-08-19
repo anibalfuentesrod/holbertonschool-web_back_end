ce02da286c9920ac926fad500ac5a9b1beecc11a
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-19 15:05:51 -0400

    making annotations on the code

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """async generator task 0"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """function waits 1 second then yield a random number(0, 10)"""
     for _ in range(10):
         await asyncio.sleep(1)
