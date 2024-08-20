d104951e70b5134a0bce9d1acfa4d4103dba586e
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-20 11:49:19 -0400

    correctign pycodestyle

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -5,7 +5,6 @@ import csv
 import math
 
 
-
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     func returns a tuple containing the start
@@ -14,6 +13,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
+
 
 class Server:
     """Server class to paginate a database of popular baby names.
@@ -35,12 +35,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """method retrieves page from dataset"""
-            assert isinstance(page, int) and page > 0, "Page have to be positive integer."
-            assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive number."
+        """method retrieves page from dataset"""
+        assert isinstance(page, int) and page > 0, \
+            "Page have to be positive integer."
+        assert isinstance(page_size, int) and page_size > 0, \
+            "Page size must be a positive number."
 
-            start, end = index_range(page, page_size)
+        start, end = index_range(page, page_size)
 
-            dataset = self.dataset()
+        dataset = self.dataset()
 
-            return dataset[start:end]
+        return dataset[start:end]
