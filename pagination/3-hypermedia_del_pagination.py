987552d2192a30d03feddfb0757d6861b169ad54
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-20 12:16:02 -0400

    pushing task without pycodestyle

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+deletion hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict, Any
+
+
+class Server:
+    """server class to paginate a database of popular baby names."""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
+        """hypermedia pagiantion"""
+        assert isinstance(index, int) and 0 <= index < len(self.indexed_dataset()), \
+            "Index out of range"
+
+        indexed_data = self.indexed_dataset()
+        data = []
+        next_index = index
+
+        while len(data) < page_size and next_index < len(indexed_data):
+            if next_index in indexed_data:
+                data.append(indexed_data[next_index])
+            next_index += 1
+
+        return {
+            "index": index,
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data
+        }
