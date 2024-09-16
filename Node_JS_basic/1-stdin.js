dd062d5bc425ac6a909104635004b08534514793
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-09-16 10:06:52 -0400

    making task 1 program

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,21 @@
+// program tha interacts with user
+
+// displays the initial msg
+process.stdout.write('Welcome to Holberton School, what is your name?');
+
+// listen for user inputs
+process.stdin.on('data', (data) => {
+  // take away extra spaces/newlines
+  const name = data.toString().trim();
+
+  // displays the user input
+  process.stdout.write(`Your name is: ${name}\n`);
+
+  // close the stdin streams to end the program
+  process.exit();
+});
+
+// listen for program to close
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
