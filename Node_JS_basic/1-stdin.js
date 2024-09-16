a269e7a69e2712f7468526b22b7b9968ed197901
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-09-16 19:57:19 -0400

    changing completed the code

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,21 +1,11 @@
-// program that interacts with user
+console.log('Welcome to Holberton School, what is your name?');
 
-// displays the initial message
-process.stdout.write('Welcome to Holberton School, what is your name?\r');
-
-// listen for user inputs
 process.stdin.on('data', (data) => {
-  // trim any extra spaces/newlines
   const name = data.toString().trim();
-
-  // displays the user input
-  process.stdout.write(`Your name is: ${name}\r`);
-
-  // close the stdin streams to end the program
-  process.exit();
+  console.log(`Your name is: ${name}`);
+  process.exit(0);
 });
 
-// listen for program to close
 process.on('exit', () => {
-  process.stdout.write('This important software is now closing\r');
+  console.log('This important software is now closing');
 });
